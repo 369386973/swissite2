cd2d44c7dbab5830c132b574102d0c7c4caf2420
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-18 23:10:48 +0800

    Update PageAside.tsx

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -6,6 +6,67 @@ import { getPageTweet } from '@/lib/get-page-tweet'
 
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
+import React, { useEffect, useState } from 'react';
+import React from 'react';
+import ScrollToTopButton from './ScrollToTopButton';
+interface ScrollToTopButtonProps {
+  showBelow: number;
+}
+const ScrollToTopButton: React.FC<ScrollToTopButtonProps> = ({ showBelow }) => {
+  const [showButton, setShowButton] = useState(false);
+  
+  // 监听页面滚动事件，更新按钮显示状态
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > showBelow) {
+        if (!showButton) {
+          setShowButton(true);
+        }
+      } else {
+        if (showButton) {
+          setShowButton(false);
+        }
+      }
+    };
+    
+    document.addEventListener('scroll', handleScroll);
+    
+    // 清除滚动事件监听
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, [showButton, showBelow]);
+   const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  return (
+    <>
+      {showButton && (
+        <button className="scroll-to-top-button" onClick={scrollToTop}>
+          返回顶部
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTopButton;
+
+const MyComponent: React.FC = () => {
+  return (
+    <div>
+      {/* ... 其他组件内容 ... */}
+      
+      {/* 在页面底部显示回到顶部按钮 */}
+      <ScrollToTopButton showBelow={1200} />
+    </div>
+  );
+};
+
+export default MyComponent;
 
 export const PageAside: React.FC<{
   block: Block
