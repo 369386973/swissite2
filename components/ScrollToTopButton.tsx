a5cb85e69dc04adaaa387448846ab6fed99068da
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-19 00:21:00 +0800

    Update ScrollToTopButton.tsx

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './ScrollToTopButton.css';
+import './styles.module.css';
 
 const ScrollToTopButton: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
