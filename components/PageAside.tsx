d06f55b23618e10cc1c4460783aa61670929d57a
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-18 23:20:11 +0800

    Update PageAside.tsx

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -1,4 +1,4 @@
-import * as React, { useEffect, useState }  from 'react'
+import * as React from 'react'
 
 import { Block, ExtendedRecordMap } from 'notion-types'
 
@@ -7,51 +7,7 @@ import { getPageTweet } from '@/lib/get-page-tweet'
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
 import ScrollToTopButton from './ScrollToTopButton';
-interface ScrollToTopButtonProps {
-  showBelow: number;
-}
-const ScrollToTopButton: React.FC<ScrollToTopButtonProps> = ({ showBelow }) => {
-  const [showButton, setShowButton] = useState(false);
-  
-  // 监听页面滚动事件，更新按钮显示状态
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset > showBelow) {
-        if (!showButton) {
-          setShowButton(true);
-        }
-      } else {
-        if (showButton) {
-          setShowButton(false);
-        }
-      }
-    };
-    
-    document.addEventListener('scroll', handleScroll);
-    
-    // 清除滚动事件监听
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, [showButton, showBelow]);
-   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
-  return (
-    <>
-      {showButton && (
-        <button className="scroll-to-top-button" onClick={scrollToTop}>
-          返回顶部
-        </button>
-      )}
-    </>
-  );
-};
 
-export default ScrollToTopButton;
 
 const MyComponent: React.FC = () => {
   return (
