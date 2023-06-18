9a9c7a0798638c628e1c77583f18840b73b20c50
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-19 00:05:34 +0800

    Update PageAside.tsx

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -6,7 +6,7 @@ import { getPageTweet } from '@/lib/get-page-tweet'
 
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
-import MyComponent from './components/MyComponent.tsx';
+import MyComponent from './components/MyComponent.js';
 
 const App: React.FC = () => {
   return (
